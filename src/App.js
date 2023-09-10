de768050a2164548b8be81086b000362a2946a26
ankit.kumar-at-527579388740 <ankit.kumar@borderfree.io>
2023-09-10 10:25:39 +0530

    added-route-for-question-ask-in-app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Auth from "./components/Auth/Auth";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import AskQuestion from "./components/AskQuestion/AskQuestion";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/Auth" element={<Auth />} />
+          <Route path="/AskQuestion" element={<AskQuestion />} />
         </Routes>
       </Router>
     </div>
